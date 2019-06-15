d19a221741d660eeb9fa83817d2b039aa238f1c4
matiheimann <mheimann@itba.edu.ar>
2019-06-15 16:44:26 -0300

    Random lib

---
+++ src/include/random.h
@@ -0,0 +1,11 @@
+#ifndef TP_CRIPTO_RANDOM_H
+#define TP_CRIPTO_RANDOM_H
+
+#include <stdint.h>
+#include <limits.h>
+
+
+uint8_t nextChar();
+void setSeed(int64_t s);
+
+#endif
---
+++ src/random.c
@@ -0,0 +1,13 @@
+#include "random.h"
+
+int64_t seed;
+
+void setSeed(int64_t s)
+{
+    seed = (s ^ 0x5DEECE66DL) & ((1LL << 48) - 1);
+}
+
+uint8_t nextChar(){
+    seed = (seed * 0x5DEECE66DL + 0xBL) & ((1LL << 48) - 1);
+    return (uint8_t)(seed>>40);
+}
